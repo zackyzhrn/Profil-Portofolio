5e684b085075cfaed41be7b7d3e58d07dd4e2e2e
zacky <64335916+zackyzhrn@users.noreply.github.com>
2025-05-30 18:46:07 +0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
+    //const sideNav = document.body.querySelector('#sideNav');
+    //if (sideNav) {
+      //  new bootstrap.ScrollSpy(document.body, {
+        //    target: '#sideNav',
+          //  rootMargin: '0px 0px -40%',
+        //});
+    //};
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
@@ -20,6 +19,24 @@ window.addEventListener('DOMContentLoaded', event => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
             }
+        });
+    });
+
+    // Script untuk preview gambar
+    document.querySelectorAll('#licenceGallery img').forEach(function(img) {
+        img.addEventListener('click', function() {
+            document.getElementById('imgModalSrc').src = this.src;
+            var modal = new bootstrap.Modal(document.getElementById('imgModal'));
+            modal.show();
+        });
+    });
+
+    // Script untuk preview gambar Experience
+    document.querySelectorAll('#experienceGallery img').forEach(function(img) {
+        img.addEventListener('click', function() {
+            document.getElementById('imgModalSrc').src = this.src;
+            var modal = new bootstrap.Modal(document.getElementById('imgModal'));
+            modal.show();
         });
     });
 
